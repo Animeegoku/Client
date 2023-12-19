4739e10cdf06c5d8a8f4e30318b85bdd1df5b517
Chisquare7 <chijiokechibuike.f@gmail.com>
2023-12-19 21:46:43 +0100

    Added font family file for the app

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
 // eslint-disable-next-line no-unused-vars
-import LandingPageStyles from "./LandingPage.module.scss"
+import React from "react";
+// eslint-disable-next-line no-unused-vars
+import LandingPageStyles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
-  return (
-    <div>
-      <div>Landing Page</div>
-    </div>
-  )
-}
+	return (
+		<div>
+			<div>Landing Page</div>
+		</div>
+	);
+};
 
-export default LandingPage
+export default LandingPage;
